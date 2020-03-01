cc097b9b24084acd3e1b70611ddde8835869b3eb
JonasKoppen_DROS <dreaddrone@gmail.com>
2020-03-01 21:24:43 +0100

    Async with Thunks

--- src/redux/reducers/authorReducer.js
+++ src/redux/reducers/authorReducer.js
@@ -3,9 +3,7 @@ import intialState from "./intialState";
 
 export default function courseReducer(state = intialState.courses, action) {
   switch (action.type) {
-    case types.CREATE_COURSE:
-      return [...state, { ...action.course }];
-    case types.LOAD_COURSES_SUCCESS:
+    case types.LOAD_AUTHORS_SUCCESS:
       return action.courses;
     default:
       return state;
